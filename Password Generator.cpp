84aaf78decef325c553aa30da25583101926ddb9
muratcanaltun <66438521+muratcanaltun@users.noreply.github.com>
2021-03-04 15:35:34 +0300

    Add files via upload

---
+++ Password Generator.cpp
@@ -0,0 +1,28 @@
+#include <ctime>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	char characters[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	srand(time(0));
+	std::cout << "How many characters will your password have?" << std::endl;
+	
+	int letters;
+	std::cin >> letters;
+
+	std::cout << "How many different choices do you want?" << std::endl;
+
+	int choices;
+	std::cin >> choices;
+
+	for (int i = 1; i <= choices; i++)
+	{
+		for (int j = 0; j < letters; j++)
+		{
+			std::cout << characters[rand() % (sizeof(characters) - 1)];
+		}
+		std::cout << "" << std::endl;
+	}
+
+	return 0;
+}
